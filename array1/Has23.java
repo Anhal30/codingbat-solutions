e0366702d217f05c14afacc31accedd0e249622d
Anhal Alruziqi <anhal9455@gmail.com>
2025-08-06 12:55:32 +0400

    Add solution for array1-has23

---
+++ array1/Has23.java
@@ -0,0 +1,11 @@
+public class Has23 {
+    public static boolean has23(int[] nums) {
+        return (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(has23(new int[]{2, 5})); // true
+        System.out.println(has23(new int[]{4, 3})); // true
+        System.out.println(has23(new int[]{4, 5})); // false
+    }
+}
