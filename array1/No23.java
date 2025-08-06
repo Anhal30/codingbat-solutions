854d301e4be8bebc85ae7c096289fa980d43bd3c
Anhal Alruziqi <anhal9455@gmail.com>
2025-08-06 12:57:16 +0400

    Add solution for array1-no23

---
+++ array1/No23.java
@@ -0,0 +1,11 @@
+public class No23 {
+    public static boolean no23(int[] nums) {
+        return (nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(no23(new int[]{4, 5})); // true
+        System.out.println(no23(new int[]{4, 2})); // false
+        System.out.println(no23(new int[]{3, 5})); // false
+    }
+}
